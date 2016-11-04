aece875a3064a4c52a5f41457b861bf487fb8eec
Veux <qwerqweab@gmail.com>
2016-11-04 03:57:31 -0700

    Fix window init

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 int main(void)
 {
   GLuint i = 0;
-
+  int flags = SDL_WINDOW_OPENGL | SDL_RENDERER_PRESENTVSYNC;
   SDL_Init(SDL_INIT_EVERYTHING);
-  auto window = SDL_CreateWindow("title", 0, 0, 320, 240, 0);
+  auto window = SDL_CreateWindow("title", 0, 0, 320, 240, flags);
   auto context = SDL_GL_CreateContext(window);
   glewInit();
   glClearColor(0, 1, 0, 1);
   glClear(GL_COLOR_BUFFER_BIT);
-  SDL_GL_SwapWindow(window);
   while (1)
   {
+    SDL_Delay(10);
+    SDL_GL_SwapWindow(window);
   }
 
   return i;
