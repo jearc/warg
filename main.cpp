e5bac0fc117d2e4999bcba7d1d9e0aeee23ebed1
James <akajea@gmail.com>
2016-11-05 00:12:08 +1100

    Add window close functionality

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 int main(void)
 {
   int flags = SDL_WINDOW_OPENGL | SDL_RENDERER_PRESENTVSYNC;
+
   SDL_Init(SDL_INIT_EVERYTHING);
   auto window = SDL_CreateWindow("title", 0, 0, 320, 240, flags);
   auto context = SDL_GL_CreateContext(window);
+
   glewInit();
   glClearColor(0, 1, 0, 1);
   glClear(GL_COLOR_BUFFER_BIT);
-  while (1)
+
+  SDL_Event event;
+  while (true)
   {
+    while (SDL_PollEvent(&event) != 0)
+      if (event.type == SDL_QUIT)
+        goto quit;
     SDL_Delay(10);
     SDL_GL_SwapWindow(window);
   }
 
+quit:
+  SDL_Quit();
   return 0;
 }
