3078efde4e6703cc44127c9bc57e34a25cc89a02
James Archer <akajea@gmail.com>
2020-08-18 01:12:41 +1000

    Create a Python wrapper class for Moov process

---
+++ moov.py
@@ -0,0 +1,123 @@
+import subprocess
+import struct
+import threading
+import queue
+import time
+
+
+def write(file, format, *args):
+	for i, c in enumerate(format):
+		if c == 'B':
+			file.write(struct.pack(f'=B', args[i]))
+		elif c == 's':
+			file.write(
+			    struct.pack(
+			        f'=I{len(args[i])}s', len(args[i]),
+			        bytes(args[i], encoding='utf-8')))
+		elif c == 'Q':
+			file.write(struct.pack('=Q', args[i]))
+		elif c == 'd':
+			file.write(struct.pack('=d', args[i]))
+		elif c == 'I':
+			file.write(struct.pack('=I', args[i]))
+
+
+def read(file, format):
+	data = []
+	for c in format:
+		if c == 'B':
+			data.append(int.from_bytes(file.read(1), 'little'))
+		elif c == 's':
+			length = int.from_bytes(file.read(4), 'little')
+			data.append(file.read(length).decode('utf-8'))
+		elif c == 'Q':
+			data.append(int.from_bytes(file.read(8), 'little'))
+		elif c == 'd':
+			data.append(struct.unpack('d', file.read(8))[0])
+		elif c == 'I':
+			data.append(int.from_bytes(file.read(4), 'little'))
+	return data
+
+
+class Moov:
+
+	def __init__(self):
+		self._status_request_counter = 0
+		self._proc = subprocess.Popen(
+		    ['./moov'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		self._message_queue = queue.Queue()
+		self._replies = dict()
+		self._replies_lock = threading.Lock()
+		self._reader_thread = threading.Thread(target=self._reader)
+		self._reader_thread.start()
+
+	def _read(self, format):
+		return read(self._proc.stdout, format)
+
+	def _write(self, format, *args):
+		write(self._proc.stdin, format, *args)
+		self._proc.stdin.flush()
+
+	def _reader(self):
+		while self._proc and self._proc.poll() is None:
+			cmd = self._read("B")[0]
+			if cmd == 4:
+				data = self._read("IQQdB")
+				with self._replies_lock:
+					self._replies[data[0]] = {
+					    "pl_pos": data[1],
+					    "pl_count": data[2],
+					    "time": data[3],
+					    "paused": data[4]
+					}
+			if cmd == 5:
+				msg = self._read("s")[0]
+				self._message_queue.put(msg)
+
+	def _request_status(self):
+		request_id = self._status_request_counter
+		self._status_request_counter += 1
+		self._write('BI', 8, request_id)
+		return request_id
+
+	def _await_reply(self, request_id):
+		while True:
+			with self._replies_lock:
+				if request_id in self._replies:
+					break
+			time.sleep(0.01)
+		reply = None
+		with self._replies_lock:
+			reply = self._replies[request_id]
+			del self._replies[request_id]
+		return reply
+
+	def alive(self):
+		return self._proc and self._proc.poll() is None
+
+	def put_message(self, message, fg_color, bg_color):
+		self._write('BIIs', 3, fg_color, bg_color, message)
+
+	def index(self, position):
+		self._write('BQ', 7, position)
+
+	def append(self, path):
+		self._write('Bs', 6, path)
+
+	def play(self):
+		self._write('BB', 1, 0)
+
+	def pause(self):
+		self._write('BB', 1, 1)
+
+	def seek(self, time):
+		self._write('Bd', 2, time)
+
+	def get_status(self):
+		request_id = self._request_status()
+		return self._await_reply(request_id)
+
+	def get_user_inputs(self):
+		inputs = list(self._message_queue.queue)
+		self._message_queue.queue.clear()
+		return inputs
--- test.py
+++ test.py
@@ -1,71 +1,7 @@
 #!/usr/bin/env python3
 
-import subprocess
-import struct
 import re
-import sys
-import threading, queue
-
-
-def read_int(n_bytes):
-	return int.from_bytes(moov.stdout.read(n_bytes), 'little')
-
-
-def read_str(n_bytes):
-	return moov.stdout.read(n_bytes).decode('utf-8')
-
-
-def read_double():
-	return struct.unpack('d', moov.stdout.read(8))[0]
-
-
-def play():
-	moov.stdin.write(bytes([1, 0]))
-	moov.stdin.flush()
-
-
-def pause():
-	moov.stdin.write(bytes([1, 1]))
-	moov.stdin.flush()
-
-
-def seek(time):
-	moov.stdin.write(bytes([2]))
-	moov.stdin.write(struct.pack('<d', time))
-	moov.stdin.flush()
-
-
-status_request_id = 0
-
-
-def request_status():
-	global status_request_id
-	moov.stdin.write(bytes([8]))
-	moov.stdin.write(struct.pack('<I', status_request_id))
-	moov.stdin.flush()
-	status_request_id += 1
-
-
-def put_chat_message(msg, fg, bg):
-	moov.stdin.write(bytes([3]))
-	moov.stdin.write(struct.pack('<I', fg))
-	moov.stdin.write(struct.pack('<I', bg))
-	moov.stdin.write(struct.pack('<I', len(msg)))
-	moov.stdin.write(bytes(msg, encoding='utf-8'))
-	moov.stdin.flush()
-
-
-def set_playlist_position(pos):
-	moov.stdin.write(bytes([7]))
-	moov.stdin.write(struct.pack('<Q', pos))
-	moov.stdin.flush()
-
-
-def add_file(path):
-	moov.stdin.write(bytes([6]))
-	moov.stdin.write(struct.pack('<I', len(path)))
-	moov.stdin.write(bytes(path, encoding='utf-8'))
-	moov.stdin.flush()
+from moov import Moov
 
 
 def parse_time(time_str):
@@ -92,27 +28,11 @@ def format_time(seconds):
 	return time_str
 
 
-def mhf():
-	while moov and moov.poll() is None:
-		cmd = read_int(1)
-		if cmd == 4:
-			request_id = read_int(4)
-			pl_pos = read_int(8)
-			pl_count = read_int(8)
-			time = read_double()
-			paused = read_int(1)
-			sq.put(
-			    {
-			        "id": request_id,
-			        "pl_pos": pl_pos,
-			        "pl_count": pl_count,
-			        "time": time,
-			        "paused": paused
-			    })
-		if cmd == 5:
-			strlen = read_int(4)
-			msg = read_str(strlen)
-			mq.put(msg)
+def format_status(status):
+	s = f'{status["pl_pos"]+1}/{status["pl_count"]} '
+	s += f'{"paused" if status["paused"] else "playing"} '
+	s += f'{format_time(status["time"])}'
+	return s
 
 
 files = [
@@ -122,67 +42,49 @@ files = [
 start_time = 5940
 start_pos = 1
 
-moov = subprocess.Popen(
-    ['./moov'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
+moov = Moov()
+
 for f in files:
-	add_file(f)
-set_playlist_position(start_pos)
-seek(start_time)
+	moov.append(f)
+moov.index(start_pos)
+moov.seek(start_time)
 
-sq = queue.Queue()
-mq = queue.Queue()
-mt = threading.Thread(target=mhf, args=())
-mt.start()
+while moov.alive():
+	messages = moov.get_user_inputs()
+	for msg in messages:
+		moov.put_message(msg, 0xfff0cf89, 0xbb000000)
 
-
-def pp():
-	request_status()
-	status = sq.get()
-	paused = status["paused"]
-	if paused:
-		play()
-	else:
-		pause()
-
-
-def get_status():
-	request_status()
-	return sq.get()
-
-
-def format_status(status):
-	s = f'{status["pl_pos"]+1}/{status["pl_count"]} '
-	s += f'{"paused" if status["paused"] else "playing"} '
-	s += f'{format_time(status["time"])}'
-	return s
-
-
-while moov and moov.poll() is None:
-	if not mq.empty():
-		msg = mq.get()
-		put_chat_message(msg, 0xfff0cf89, 0xbb000000)
 		if msg == "status":
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
+
 		if msg == "pp":
-			pp()
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			status = moov.get_status()
+			if status["paused"]:
+				moov.play()
+			else:
+				moov.pause()
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
+
 		if msg == "play":
-			play()
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			moov.play()
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
+
 		if msg == "pause":
-			pause()
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			moov.pause()
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
+
 		if msg[0:4] == "seek":
-			seek(parse_time(msg[5:]))
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			moov.seek(parse_time(msg[5:]))
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
+
 		if msg[0:5] == "index":
 			prog = re.compile(r'(-?\d+)')
 			nums = prog.findall(msg[6:])
-			set_playlist_position(int(nums[0]) - 1)
-			status = format_status(get_status())
-			put_chat_message(status, 0xff00ffff, 0xbb000000)
+			moov.index(int(nums[0]) - 1)
+			status = moov.get_status()
+			moov.put_message(format_status(status), 0xff00ffff, 0xbb000000)
